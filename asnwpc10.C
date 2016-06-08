aacffe27fd5469308a8eda90d6200005d58be69d
Brook Zou <brook.zou@sap.com>
2016-06-08 18:27:49 +0800

    2016/06/08 18:27

--- asnwpc10.C
+++ asnwpc10.C
@@ -3,7 +3,7 @@
 // OCO Source Materials
 // 5655-DRP
 // Copyright IBM Corp. 2013.
-//add this line for git test
+//add for test 
 // The source code for this program is not published or otherwise divested of 
 // its trade secrets, irrespective of what has been deposited with the U.S. 
 // Copyright Office.
--- asnwpc_201501010000.C
+++ asnwpc_201501010000.C
@@ -3,7 +3,7 @@
 // OCO Source Materials
 // 5655-DRP
 // Copyright IBM Corp. 2013.
-//
+// added for test for test for test for test
 // The source code for this program is not published or otherwise divested of 
 // its trade secrets, irrespective of what has been deposited with the U.S. 
 // Copyright Office.
